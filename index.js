1229440d90539c5a10df5d0c93ac95ae634edcb0
Arib Farooqui <aribbabar100@gmail.com>
2021-06-10 22:26:38 -0400

    Added school projects page

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
 function animateSkills() {
     let skills = document.getElementsByClassName("skill-container");
+    let counter = 0;
 
-    document.addEventListener("scroll", function() {
+    let elementsDone = new Set();
 
+    document.addEventListener("scroll", scrollCheck);
+
+    function scrollCheck() {
         for(let i = 0;i < skills.length;i++) {
             if(skills[i].getBoundingClientRect().top - window.innerHeight <= 0) {
-                setTimeout(setWidth, 500, skills[i]);
+                setTimeout(setWidth, 100, skills[i], i);
             }
         }
-    });
 
-    function setWidth(element) {
+        // Check if all widths are set, if so, remove the scroll event listener
+        if(elementsDone.size == 10) {
+            document.removeEventListener("scroll", scrollCheck);
+        }
+    }
+
+    function setWidth(element, index) {
         switch(element.firstElementChild.innerHTML) {
             case "Java":
                 element.lastElementChild.firstElementChild.style.width = "90%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "C#":
-                element.lastElementChild.firstElementChild.style.width = "85%";
+                element.lastElementChild.firstElementChild.style.width = "80%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "C":
                 element.lastElementChild.firstElementChild.style.width = "90%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "HTML &amp; CSS":
                 element.lastElementChild.firstElementChild.style.width = "60%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "Vanilla JavaScript":
                 element.lastElementChild.firstElementChild.style.width = "50%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "Basic Graphic Designing":
-                element.lastElementChild.firstElementChild.style.width = "50%";
+                element.lastElementChild.firstElementChild.style.width = "40%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
 
             case "Positivity":
                 element.lastElementChild.firstElementChild.style.width = "100%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "Adaptability":
                 element.lastElementChild.firstElementChild.style.width = "80%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "Teamwork":
                 element.lastElementChild.firstElementChild.style.width = "85%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
             case "Communication":
                 element.lastElementChild.firstElementChild.style.width = "75%";
+                elementsDone.add(element.firstElementChild.innerHTML);
                 break;
 
             default:
